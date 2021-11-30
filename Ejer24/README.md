1162920db7af78b19eee1aaf7431caf013818082
Nailalvz <suareznaila@gmail.com>
2021-11-30 19:41:16 +0100

    Ejercicio 24 resuelto

---
+++ Ejer24/README.md
@@ -0,0 +1,1 @@
+24. Build. Automatiza el proceso de compilación de ejecutable y biblioteca, su enlazado y la generación del archivo .jar para código fuente en JAva con Maven. Haz uso de un buildfile.
---
+++ Ejer24/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+      // Prueba vacía
+  }
+
+}
